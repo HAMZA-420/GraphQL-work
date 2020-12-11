73039752f85a02e4dca10934f1a00c5658e7aedd
MUHAMMAD HAMZA KHAN <56793985+HAMZA-420@users.noreply.github.com>
2020-12-11 20:18:51 +0500

    Graphql schema

--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 
 const app = express();
+app.use('/graphql',graphqlHTTP({
+    
+}));
 
 app.listen(4000,()=> {
     console.log('now listening for request on port 4000');
