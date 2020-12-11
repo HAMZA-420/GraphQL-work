77983169978f4828a23c883136c86f784d514389
MUHAMMAD HAMZA KHAN <56793985+HAMZA-420@users.noreply.github.com>
2020-12-11 22:04:25 +0500

    testing queries on graphiql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,14 +4,14 @@ const_ = require('lodash');
 
 //dummy data
 var books = [
-    {name: 'Name of the Wind', genre: 'Fantasy', id:'1'},
+    {name: "Name of the Wind", genre: "Fantasy", id:"1"},
     {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
     {name: 'The Long Earth', genre: 'Sci-Fi', id: '3'},
 ];
 
 const BookType = new GraphQLObjectType({
     name:'Book',
-    fields:()=>({
+    fields: ()=>({
         id:{type: GraphQLString},
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
@@ -26,7 +26,7 @@ const RootQuery = new GraphQLObjectType({
             args: {id:{type: GraphQLString}},
             resolve(parent,args) {
                 //code to get data from db/other source
-                return _.find(books,{ id: args.id});
+                return _.find(books,{ id:args.id});
 
             }
         }
