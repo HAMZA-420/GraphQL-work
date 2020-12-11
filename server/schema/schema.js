ea3391a7bd3705466135f825f5708ab63c912ae2
MUHAMMAD HAMZA KHAN <56793985+HAMZA-420@users.noreply.github.com>
2020-12-11 20:30:40 +0500

    Root Query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,5 @@
 const graphql  = require('graphql');
-const{GraphQLObjectType, GraphQLString} = graphql;
+const{GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
 
 const BookType = new GraphQLObjectType({
     name:'Book',
@@ -8,4 +8,20 @@ const BookType = new GraphQLObjectType({
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
     })
-})
+});
+
+const RootQuery = new GraphQLObjectType({
+    name:'RootQueryType',
+    fields: {
+        book:{
+            type:BookType,
+            args: {id:{type: GraphQLString}},
+            resolve(parent,args) {
+                //code to get data from db/other source
+
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema;
