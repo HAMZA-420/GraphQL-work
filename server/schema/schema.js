1de6682c99202dc6097d28d57e803903c1d7e168
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-08-05 01:51:19 +0500

    updating resolve functions

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -17,7 +17,8 @@ const BookType = new GraphQLObjectType({
             resolve(parent, args) {
                 console.log(parent);
           //      return _.find(authors,{id: parent.authorId});
-            }
+            return Author.findById(parent.authorId);
+        }
         }
     })
 });
@@ -32,7 +33,8 @@ const AuthorType = new GraphQLObjectType({
             type: new GraphQLList(BookType),
             resolve(parent,args) {
          //       return _.filter(books,{authorId:parent.id});
-            }
+            return Book.find({authorId: parent.id}) 
+        }
         }
     })
 });
@@ -47,6 +49,7 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent,args) {
                 //code to get data from db/other source
             //    return _.find(books,{id:args.id});
+            return Book.findById(args.id)
 
             }
         }, 
@@ -55,18 +58,21 @@ const RootQuery = new GraphQLObjectType({
             args:{id:{type: GraphQLID}},
             resolve(parent, args){
             //    return _.find(authors, {id:args.id});
+            return Author.findById(args.id)
             }
         },
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args){
             //    return books
+            return Book.find({})
             }
         },
         authors: {
             type: new GraphQLList(AuthorType),
             resolve(parent,args){
             //    return authors;
+            return Author.find({})
 
             }
 
