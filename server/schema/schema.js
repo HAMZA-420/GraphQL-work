0f08f79f614780f5d15b903c08706221dabd7969
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-08-05 02:01:39 +0500

    Adding GraphQLNonNull

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,5 @@
 const graphql  = require('graphql');
-const {GraphQLObjectType,GraphQLList, GraphQLString, GraphQLSchema,GraphQLID , GraphQLInt} = graphql;
+const {GraphQLObjectType,GraphQLList, GraphQLString, GraphQLSchema,GraphQLID , GraphQLInt, GraphQLNonNull} = graphql;
 const _ = require('lodash');
 const Book = require('../models/book');
 const Author = require('../models/author');
@@ -88,8 +88,8 @@ const Mutation = new GraphQLObjectType({
         addAuthor: {
             type: AuthorType,
             args: {
-                name: {type: GraphQLString},
-                age: {type: GraphQLInt}
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                age: {type: new GraphQLNonNull(GraphQLInt)}
             },
             resolve(parent, args) {
             let author = new Author({
@@ -102,9 +102,9 @@ const Mutation = new GraphQLObjectType({
         addBook: {
             type: BookType,
             args: {
-                name: {type: GraphQLString},
-                genre: {type: GraphQLString},
-                authorId: {type: GraphQLID}
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                genre: {type: new GraphQLNonNull(GraphQLString)},
+                authorId: {type: new GraphQLNonNull(GraphQLID)}
             },
             resolve(parent, args) {
                 let book = new Book({
