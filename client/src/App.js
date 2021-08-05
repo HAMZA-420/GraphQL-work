ec1f09483df6846b1c006c54248998fe80ce47c7
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-08-05 22:07:49 +0500

    AddBook Component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import BookList from "./components/BookList";
 import ApolloCleint from 'apollo-boost';
 import {ApolloProvider} from "react-apollo";
+import AddBook from "./components/AddBook";
 
 const client = new ApolloCleint({
   uri: "http://localhost:4000/graphql"
@@ -13,6 +14,7 @@ function App() {
     <div id="main">
       <h1>Welcome to Book List</h1>
       <BookList />
+      <AddBook />
     </div>
     </ApolloProvider>
   );
---
+++ client/src/components/AddBook.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const getAuthorsQuery = gql`
+    {
+        authors {
+            name
+            id
+        }
+    }
+`;
+
+function AddBook(props) {
+    function displayAuthors(){
+        var data = props.data;
+        if(data.loading){
+            return( <option disabled>Loading authors</option> );
+        } else {
+            return data.authors.map(author => {
+                return( <option key={ author.id } value={author.id}>{ author.name }</option> );
+            });
+        }
+    }
+        return(
+            <form id="add-book">
+                <div className="field">
+                    <label>Book name:</label>
+                    <input type="text" />
+                </div>
+                <div className="field">
+                    <label>Genre:</label>
+                    <input type="text" />
+                </div>
+                <div className="field">
+                    <label>Author:</label>
+                    <select>
+                        <option>Select author</option>
+                        { displayAuthors() }
+                    </select>
+                </div>
+                <button>+</button>
+
+            </form>
+        );
+    }
+
+export default graphql(getAuthorsQuery)(AddBook);
