a800d582f314bac1d9bc71e7d536938b8303e3fa
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-08-05 12:07:25 +0500

    ApolloClient setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React, {Component} from "react";
 import BookList from "./components/BookList";
+import ApolloCleint from 'apollo-boost';
+import {ApolloProvider} from "react-apollo";
+
+const client = new ApolloCleint({
+  uri: "http://localhost:4000/graphql";
+})
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div id="main">
       <h1>Welcome to Book List</h1>
       <BookList />
     </div>
+    </ApolloProvider>
   );
 }
 
