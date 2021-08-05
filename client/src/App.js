ebf983b0413ea95d1f2630a5ae9eb923b40c2ebf
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-08-05 12:00:23 +0500

    Book-list Componet

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from "react";
-
+import BookList from "./components/BookList";
 
 function App() {
   return (
     <div id="main">
       <h1>Welcome to Book List</h1>
+      <BookList />
     </div>
   );
 }
---
+++ client/src/components/BookList.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+
+function BookList() {
+  return (
+    <div >
+      <ul id="book-list">
+          <li>Book name</li>
+      </ul>
+    </div>
+  );
+}
+
+export default BookList;
